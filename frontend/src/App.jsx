f2f9bd0ff2c01eac73ff090eee2ab49e336baad0
Iman Maulana <maulanaiman902@gmail.com>
2024-10-23 20:32:28 +0700

    fix: set timing close toast notification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     <>
       <ToastContainer
         position='top-center'
-        autoClose={5000}
+        autoClose={3000}
         pauseOnHover={false}
         closeOnClick
         newestOnTop
